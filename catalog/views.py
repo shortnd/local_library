adc2a0bb6e067bf7535c62b67275697200e524c9
Collin <ococncol@gmail.com>
2019-06-14 00:12:04 -0400

    at pagination

--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from .models import (
-  Book,
-  Author,
-  BookInstance,
-  Genre
+    Book,
+    Author,
+    BookInstance,
+    Genre
 )
 
 # Create your views here.
+
+
 def index(request):
     """ View function for home page of site. """
     num_books = Book.objects.all().count()
@@ -14,22 +17,34 @@ def index(request):
 
     # Avaliable books (status = 'a')
     num_instances_avaliable = BookInstance.objects.filter(
-      status__exact='a'
+        status__exact='a'
     ).count()
 
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
     num_genres = Genre.objects.filter(
-      name__contains='Sci'
+        name__contains='Sci'
     ).count()
 
     context = {
-      'num_books': num_books,
-      'num_instances': num_instances,
-      'num_instances_avaliable': num_instances_avaliable,
-      'num_authors': num_authors,
-      'num_genres': num_genres,
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_avaliable': num_instances_avaliable,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
     }
 
     return render(request, 'index.html', context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'my_book_list'  # your own name for the list as a template valiable
+    # queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the war
+    template_name = 'book_list.html' # Specify your own template name/location
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'book_detail.html'
